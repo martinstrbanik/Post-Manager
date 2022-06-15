e4ec1f9fbb2bd9dc4bf35b57a05f4a3fd7e86985
Martin3 <strbanik.jun@gmail.com>
2022-06-15 13:14:04 +0200

    Added spring boot project

---
+++ src/main/java/com/strbanik/PostManager/PostManagerApplication.java
@@ -0,0 +1,14 @@
+package com.strbanik.PostManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManagerApplication.class, args);
+	}
+
+
+}
